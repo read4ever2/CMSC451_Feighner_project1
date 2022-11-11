ca92775265cef3fb73b988d11cca09731d550412
read4ever2 <62565368+read4ever2@users.noreply.github.com>
2022-11-11 11:00:27 -0700

    updated documentation

--- src/project1/MergeSort.java
+++ src/project1/MergeSort.java
@@ -1,5 +1,15 @@
+/*
+ * Filename: BenchmarkSorts.java
+ * Author: Will Feighner
+ * Date: 2022 11 06
+ * Purpose: This program analyses the efficiency of iterative and recursive versions
+ * the Merge Sort algorithm by generating random data to be sorted, timing
+ * how long it takes, and counting the number operations the algorithm takes.
+ */
+
 package project1;
 
+// Imports
 import java.util.Arrays;
 
 public class MergeSort implements SortInterface {
--- src/project1/SortInterface.java
+++ src/project1/SortInterface.java
@@ -1,3 +1,12 @@
+/*
+ * Filename: BenchmarkSorts.java
+ * Author: Will Feighner
+ * Date: 2022 11 06
+ * Purpose: This program analyses the efficiency of iterative and recursive versions
+ * the Merge Sort algorithm by generating random data to be sorted, timing
+ * how long it takes, and counting the number operations the algorithm takes.
+ */
+
 package project1;
 
 public interface SortInterface {
