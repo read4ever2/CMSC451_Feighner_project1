42d6b62ad10586e9251c4f2e1ae621d987260871
read4ever2 <62565368+read4ever2@users.noreply.github.com>
2022-11-11 11:59:13 -0700

    csv output test

--- src/project1/BenchmarkSorts.java
+++ src/project1/BenchmarkSorts.java
@@ -11,8 +11,15 @@ package project1;
 
 // Imports
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class BenchmarkSorts {
 
@@ -39,7 +46,43 @@ public class BenchmarkSorts {
 
     System.out.println(Arrays.deepToString(benchmarkSortsObject.getFinalRecursiveReportOutput()));
     System.out.println(Arrays.deepToString(benchmarkSortsObject.getFinalIterativeReportOutput()));
+
+    List<String[]> dataLines = new ArrayList<>();
+    dataLines.add(new String[]
+            { "John", "Doe", "38", "Comment Data\nAnother line of comment data" });
+    dataLines.add(new String[]
+            { "Jane", "Doe, Jr.", "19", "She said \"I'm being quoted\"" });
+    try {
+      benchmarkSortsObject.givenDataArray_whenConvertToCSV_thenOutputCreated(dataLines);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
   } // End main
+
+  public String convertToCSV(String[] data) {
+    return Stream.of(data)
+            .map(this::escapeSpecialCharacters)
+            .collect(Collectors.joining(","));
+  }
+
+  public void givenDataArray_whenConvertToCSV_thenOutputCreated(List<String[]> dataLines) throws IOException {
+    File csvOutputFile = new File("CSV_FILE_NAME.csv");
+    try (PrintWriter pw = new PrintWriter(csvOutputFile)) {
+      dataLines.stream()
+              .map(this::convertToCSV)
+              .forEach(pw::println);
+    }
+  }
+
+  public String escapeSpecialCharacters(String data) {
+    String escapedData = data.replaceAll("\\R", " ");
+    if (data.contains(",") || data.contains("\"") || data.contains("'")) {
+      data = data.replace("\"", "\"\"");
+      escapedData = "\"" + data + "\"";
+    }
+    return escapedData;
+  }
+
 
   private long[][] getFinalIterativeReportOutput() {
     return finalIterativeReportOutput;
@@ -159,6 +202,10 @@ public class BenchmarkSorts {
     this.dataSource = dataSource;
   }
 
+  /**
+   * @param length number of random elements to generate for each run
+   * @return 2d array of random integers
+   */
   private int[][] generateData(int length) {
 
     Random random = new Random();
