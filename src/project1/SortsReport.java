f730129303e8e95622931f96f9f6db0bd6d0dcce
read4ever2 <62565368+read4ever2@users.noreply.github.com>
2022-11-12 16:00:16 -0700

    warmup test

--- src/project1/SortsReport.java
+++ src/project1/SortsReport.java
@@ -12,25 +12,53 @@ package project1;
 import javax.swing.*;
 import java.util.Arrays;
 
-public class SortsReport  {
+public class SortsReport {
 
+  // Fields
+  private int[][] reportData;
+  private int[][] displayData;
 
-  public static void main(String[] args) {
-    int[][] testData = testData();
-    displayGUI(testData);
+  public SortsReport() {
+    reportData = new int[0][];
+    displayData = new int[10][];
   }
 
-  private static void displayGUI(int[][] data) {
+  public static void main(String[] args) {
+
+    SortsReport sortsReport = new SortsReport();
+
+    sortsReport.setDisplayData(sortsReport.testData());
+    sortsReport.displayGUI(sortsReport.getDisplayData());
+  }
+
+  /**
+   * @return Sample Display Data
+   */
+  private int[][] testData() {
+    int[][] sampleData = new int[10][5];
+    for (int i = 0; i < sampleData.length; i++) {
+      for (int j = 0; j < sampleData[i].length; j++) {
+        sampleData[i][j] = i + j;
+      }
+    }
+    System.out.println(Arrays.deepToString(sampleData));
+    return sampleData;
+  }
+
+  /**
+   * @param data Input data to display
+   */
+  private void displayGUI(int[][] data) {
     JFrame jFrame = new JFrame();
     JTable jTable;
 
     jFrame.setTitle("Benchmark Report");
 
-    Object[][] objectData = new Object[10][5];
+    Object[][] objectData = new Object[data.length][data[0].length];
 
     for (int i = 0; i < data.length; i++) {
       for (int j = 0; j < data[i].length; j++) {
-        objectData[i][j]= data[i][j];
+        objectData[i][j] = data[i][j];
       }
     }
     String[] columnNames = {"Size", "Avg Count", "Coef Count", "Avg Time", "Coef Time"};
@@ -46,14 +74,19 @@ public class SortsReport  {
 
   }
 
-  private static int[][] testData() {
-    int[][] sampleData = new int[10][5];
-    for (int i = 0; i < sampleData.length; i++) {
-      for (int j = 0; j < sampleData[i].length; j++) {
-        sampleData[i][j] = i + j;
-      }
-    }
-    System.out.println(Arrays.deepToString(sampleData));
-    return sampleData;
+  public int[][] getReportData() {
+    return reportData;
+  }
+
+  public void setReportData(int[][] reportData) {
+    this.reportData = reportData;
+  }
+
+  public int[][] getDisplayData() {
+    return displayData;
+  }
+
+  public void setDisplayData(int[][] displayData) {
+    this.displayData = displayData;
   }
 }
