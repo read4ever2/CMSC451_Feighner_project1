5b561f2821fa29313b998dc3e74dcd7900d7a8b7
read4ever2 <62565368+read4ever2@users.noreply.github.com>
2022-11-12 18:44:40 -0700

    file read test

--- src/project1/SortsReport.java
+++ src/project1/SortsReport.java
@@ -10,7 +10,10 @@
 package project1;
 
 import javax.swing.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class SortsReport {
 
@@ -27,8 +30,44 @@ public class SortsReport {
 
     SortsReport sortsReport = new SortsReport();
 
+    sortsReport.setReportData(sortsReport.readData());
+    System.out.println(Arrays.deepToString(sortsReport.getReportData()));
+
     sortsReport.setDisplayData(sortsReport.testData());
     sortsReport.displayGUI(sortsReport.getDisplayData());
+  }
+
+  private int[][] readData() {
+    JFileChooser jFileChooser = new JFileChooser();
+    jFileChooser.showOpenDialog(null);
+    File file = new File(jFileChooser.getSelectedFile().toURI());
+
+    // System.out.println(file);
+
+    String[][] draftFileData = new String[10][101];
+
+    try {
+      Scanner scanner = new Scanner(file);
+
+      int counter = 0;
+      while (scanner.hasNextLine()) {
+        String line = scanner.nextLine();
+        draftFileData[counter] = line.split(",");
+        counter++;
+      }
+    } catch (FileNotFoundException fileNotFoundException) {
+      System.out.println("An error occurred.");
+      fileNotFoundException.printStackTrace();
+    }
+
+    int[][] returnFileData = new int[draftFileData.length][draftFileData[0].length];
+
+    for (int i = 0; i < draftFileData.length; i++) {
+      for (int j = 0; j < draftFileData[i].length; j++) {
+        returnFileData[i][j] = Integer.parseInt(draftFileData[i][j]);
+      }
+    }
+    return returnFileData;
   }
 
   /**
@@ -38,10 +77,10 @@ public class SortsReport {
     int[][] sampleData = new int[10][5];
     for (int i = 0; i < sampleData.length; i++) {
       for (int j = 0; j < sampleData[i].length; j++) {
-        sampleData[i][j] = i + j;
+        sampleData[i][j] = (i + 1) * (j + 1);
       }
     }
-    System.out.println(Arrays.deepToString(sampleData));
+    // System.out.println(Arrays.deepToString(sampleData));
     return sampleData;
   }
 
